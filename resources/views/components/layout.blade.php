df690e5f993f9acd12e6e6acad7faf0c4bc44a2a
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2023-04-05 11:54:12 +0500

    intial commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/app.css">
+
+    <title>Blog</title>
+</head>
+
+<body>
+
+    {{ $slot }}
+
+
+</body>
+
+</html>
